49918e6679ecfce4a19a2b75ef90f926b3792a34
Lorelin-usf <70179565+Lorelin-usf@users.noreply.github.com>
2020-09-07 20:20:08 -0400

    Add files via upload

---
+++ TN Rural VS Urban Census (1).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+import pandas as pd 
+   
+
+
+# In[23]:
+
+
+df = pd.read_csv('all_040_in_47.P2.csv')
+
+
+# In[24]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
